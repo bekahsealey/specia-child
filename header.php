4525a918d154d586d7c47127991962cbdb163b33
bekahsealey <bekah@bekahsealey.com>
2017-01-14 10:44:47 -0600

    begin header typography

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class('constitution'); ?>>
+<body <?php body_class('constitution '); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'specia' ); ?></a>
 
