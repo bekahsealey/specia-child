775f68adf7189aa79030dfede95fc3e4648475fd
Rebekah Sealey <bekah@bekahsealey.com>
2017-01-20 17:41:51 -0600

    renamed

--- sidebar-education.php
+++
@@ -1,9 +0,0 @@
-<?php if( is_active_sidebar('specia_child_education_station_widget') ) : ?>
-<div class="row padding-bottom-30">
-	<div class="col-md-12 col-sm-12">
-		<div class="education-sidebar" role="complementary">
-			<?php dynamic_sidebar('specia_child_education_station_widget'); ?>
-		</div><!-- #secondary -->
-	</div>
-</div>
-<?php endif; ?>
