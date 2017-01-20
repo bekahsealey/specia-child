bbd1cf96e7c17535426e19d98b12ee690a32cc29
Maximilian <max@Maximilians-iMac.local>
2017-01-20 13:16:31 -0600

    moved query code from templates to functions

--- functions.php
+++ functions.php
@@ -7,9 +7,11 @@ function specia_child_enqueue_styles() {
 	wp_enqueue_script( 'my-scalem', get_stylesheet_directory_uri() . '/js/myscalem.js', array( 'js-scalem' ), '1.0.0', true );
 }
 
-// Add custom homepage image size
+// Add custom homepage and committee profile image sizes
 
 add_image_size( 'home', 360, 240, true );
+add_image_size( 'portrait', 150, 180, true );
+add_image_size( 'excerpt', 1138, 300, true );
 
 function remove_some_widgets(){
 
@@ -25,6 +27,15 @@ function specia_child_widgets_init() {
 		'name' => __( 'Features Widget Area', 'specia' ),
 		'id' => 'specia_child_feature_widget',
 		'description' => __( 'The Features Widget Area for Home Features', 'specia' ),
+		'before_widget' => '<div class="col-md-4 col-sm-4"><aside id="%1$s" class="widget">',
+		'after_widget' => '</aside></div>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3><div class="title-border"></div>',
+	) );
+	register_sidebar( array(
+		'name' => __( 'Education Station Widget Area', 'specia' ),
+		'id' => 'specia_child_education_station_widget',
+		'description' => __( 'The Education Station Widget Area', 'specia' ),
 		'before_widget' => '<div class="col-md-4 col-sm-4"><aside id="%1$s" class="widget">',
 		'after_widget' => '</aside></div>',
 		'before_title' => '<h3 class="widget-title">',
@@ -207,5 +218,48 @@ function my_social_icons() {
 		return;
 	}
 }
+
+function modify_the_query( $query ) {
+	if ( $query->is_post_type_archive( 'const_chairman' ) ) { // Run only on chairman messages		
+		$ppp = get_option( 'posts_per_page' );
+		$offset = 1;
+		if ( !$query->is_paged()) {	
+			$query->query_vars['posts_per_page'] = 1; 
+		} else {
+			$query->query_vars['posts_per_page'] = $ppp; 
+			$query->query_vars['offset'] 		= $offset;
+		}
+	}
+	if ( $query->is_post_type_archive( 'const_committee' ) ) {
+		$query->query_vars['orderby'] 			= 'meta_value_num';
+		$query->query_vars['order']				= 'ASC';
+		$query->query_vars['meta_key']			= 'order';
+		$query->query_vars['posts_per_page']	= -1;
+	
+	}
+}
+// Hook my above function to the pre_get_posts action
+add_action( 'pre_get_posts', 'modify_the_query' );
+add_filter( 'found_posts', 'socia_child_adjust_offset_pagination', 1, 2 );
+function socia_child_adjust_offset_pagination( $found_posts, $query ) {
+
+	// Define our offset again...
+	$offset = 1;
+
+	// Ensure we're modifying the right query object...
+	if ( $query->is_post_type_archive( 'const_chairman' ) && !$query->is_paged ) {
+		// Reduce WordPress's found_posts count by the offset...
+		return $found_posts - $offset;
+	}
+	return $found_posts;
+}
+// Replaces the excerpt "Read More" text by a link
+function new_excerpt_more($more) {
+       global $post; 
+       if (!is_home() && !is_front_page()) {
+			return '<p><a class="moretag" href="'. get_permalink($post->ID) . '">More &raquo;</a></p>';
+		}
+}
+add_filter('excerpt_more', 'new_excerpt_more');
 
 ?>
